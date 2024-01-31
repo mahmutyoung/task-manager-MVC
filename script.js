62eba845fc63876889605fb3a4df74ce97227103
mahmutyoung <mahmutyoung@gmail.com>
2024-01-31 10:44:35 +0100

    basic project with adding and deleting task features

--- script.js
+++ script.js
@@ -1,3 +1,56 @@
-import printName from './utils.js';
+const model = {
+  tasks: [],
 
-printName('Hello');
+  addTask: function (task) {
+    this.tasks.push(task);
+  },
+  deleteTask: function (task) {
+    this.tasks = this.tasks.filter((t) => t !== task);
+  },
+};
+
+const controller = {
+  init: function () {
+    console.log('init');
+    view.render();
+  },
+  handleAddTask: function (e) {
+    console.log('submit');
+    e.preventDefault();
+    const input = document.getElementById('newTask');
+    console.log(input.value);
+    model.addTask(input.value);
+    view.render();
+    input.value = '';
+  },
+  handleDeleteTask: function (idX) {
+    console.log(idX);
+    model.deleteTask(model.tasks[idX]);
+    view.render();
+  },
+};
+
+const view = {
+  render: function () {
+    const addButton = document.getElementById('addTask');
+    console.log(addTask);
+    addButton.addEventListener('click', controller.handleAddTask);
+    const ul = document.getElementById('taskList');
+    ul.innerHTML = '';
+    model.tasks.forEach((task, index) => {
+      const li = document.createElement('li');
+      li.textContent = task;
+      const button = document.createElement('button');
+      button.textContent = 'X';
+      button.addEventListener(
+        'click',
+        controller.handleDeleteTask.bind(null, index)
+      );
+
+      li.appendChild(button);
+      ul.appendChild(li);
+    });
+  },
+};
+
+controller.init();
